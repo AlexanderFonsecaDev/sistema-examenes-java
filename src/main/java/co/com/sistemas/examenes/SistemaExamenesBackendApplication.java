c79e99f863c1302caf1b94d0430cf1ea651b7574
Alexander Fonseca <jafonsec@bancolombia.com.co>
2022-08-29 14:17:11 -0500

    Inicio del proyecto

---
+++ src/main/java/co/com/sistemas/examenes/SistemaExamenesBackendApplication.java
@@ -0,0 +1,13 @@
+package co.com.sistemas.examenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaExamenesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaExamenesBackendApplication.class, args);
+	}
+
+}
